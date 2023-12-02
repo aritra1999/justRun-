c0a06eed4e0858f624c595219c858690a33aea4e
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-02 23:44:47 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import Editor from '$lib/components/editor/editor.svelte';
 	import Output from '$lib/components/io/output.svelte';
 	import Input from '$lib/components/io/input.svelte';
-	import Settings from '$lib/components/settings/settings.svelte';
 	import Menu from '$lib/components/menu/menu.svelte';
 	export let data;
 	let content = data.content;
@@ -10,14 +9,9 @@
 </script>
 
 <Menu />
-
 <div class="h-screen w-screen flex">
 	<div class="h-screen w-1/2 p-3 pr-0 bg-background">
-		{#if data.content}
-			<Editor bind:content bind:language editable={true} />
-		{:else}
-			Loading
-		{/if}
+		<Editor bind:content bind:language editable={true} />
 	</div>
 	<div class="h-screen w-1/2 flex flex-col p-3">
 		<div class=" h-1/2 pb-3">
