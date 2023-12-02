36704de9bdf1c60e5d6d1bae85dbf457ec4bd1d1
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-02 10:38:55 +0100

    Add core layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<div class="h-screen w-screen flex">
+    <div class="h-screen w-1/2 p-3 pr-0">
+        <div class="bg-blue-100 h-full w-full rounded-lg">
+
+        </div>
+    </div>
+    <div class="h-screen w-1/2 flex flex-col p-3">
+        <div class="bg-blue-100 h-20 rounded-lg mb-3">
+            Controls
+        </div>
+        <div class="h-full ">
+            <div class=" h-1/2 pb-3">
+                <div class="bg-blue-100 w-full h-full rounded-lg">
+                    Input
+                </div>
+            </div>
+            <div class="h-1/2">
+                <div class="bg-blue-100 w-full h-full rounded-lg">
+                    Output
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
