88a6e297905f521c039f9d3437e6dd3d2075a8c5
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-02 12:15:35 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import Editor from '$lib/components/editor/editor.svelte';
+	import Settings from '$lib/components/settings/settings.svelte';
 	export let data;
 	let content = data.content;
 </script>
+
+<Settings />
 
 <div class="h-screen w-screen flex">
 	<div class="h-screen w-1/2 p-3 pr-0">
@@ -14,10 +17,10 @@
 	</div>
 	<div class="h-screen w-1/2 flex flex-col p-3">
 		<div class=" h-1/2 pb-3">
-			<div class="bg-blue-100 w-full h-full rounded-lg">Input</div>
+			<div class="bg-background w-full h-full rounded-lg">Input</div>
 		</div>
 		<div class="h-1/2">
-			<div class="bg-blue-100 w-full h-full rounded-lg">Output</div>
+			<div class="bg-background w-full h-full rounded-lg">Output</div>
 		</div>
 	</div>
 </div>
