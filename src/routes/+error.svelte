00e9a37ad24aa99a52b13af4b9ada1d7cba6ad6f
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-02 15:26:15 +0100

    Create +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="h-screen w-screen flex items-center justify-center">
+	<div class="fixed text-[50vw] text-background font-black">{$page.status}</div>
+	<div class="fixed">{$page.error?.message}</div>
+</div>
