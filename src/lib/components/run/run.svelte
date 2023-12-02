965cea16003b7ca31c0656e9f688d73f327bd071
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-02 23:44:30 +0100

    Add code execution

--- src/lib/components/run/run.svelte
+++ src/lib/components/run/run.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
-	import { Play } from 'lucide-svelte';
+	import { Loader, Play } from 'lucide-svelte';
+
+	let loading = false;
+	let language = 'cpp';
+	let code = 'dummy code';
+	let input = 'dummy input';
+
+	export const runCode = async () => {
+		loading = true;
+		await fetch('/api/run', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ language, code, input })
+		})
+			.then((res) => res.json())
+			.catch((err) => console.log(err))
+			.finally(() => (loading = false));
+	};
 </script>
 
-<Button class="flex items-center text-lg" size="lg" variant="secondary">
+<Button on:click={runCode} class="flex items-center text-lg" size="lg" variant="secondary">
 	Run
-	<Play class="h-5 w-5 ml-3" />
+	{#if loading}
+		<Loader class="h-5 w-5 ml-3 animate-spin" />
+	{:else}
+		<Play class="h-5 w-5 ml-3" />
+	{/if}
 </Button>
