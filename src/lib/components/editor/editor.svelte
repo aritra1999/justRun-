883cf36d23adbd143cc5340bb8eee3d5bccd3d88
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-02 11:27:32 +0100

    Add editor component

---
+++ src/lib/components/editor/editor.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">  
+  import loader from '@monaco-editor/loader';
+  import { onDestroy, onMount } from 'svelte';
+  import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+  
+  export let content: string;
+  
+  let editor: Monaco.editor.IStandaloneCodeEditor;
+  let monaco: typeof Monaco;
+  let editorContainer: HTMLElement;
+
+  onMount(async () => {
+        const monacoEditor = await import('monaco-editor');
+        loader.config({ monaco: monacoEditor.default });
+
+        monaco = await loader.init();
+
+        const editor = monaco.editor.create(editorContainer);
+        const model = monaco.editor.createModel(content, 'cpp');
+        
+        editor.setModel(model);
+    });
+
+    onDestroy(() => {
+        monaco?.editor.getModels().forEach((model) => model.dispose());
+        editor?.dispose();
+    });
+</script>
+
+<div class="h-full w-full rounded-lg" bind:this={editorContainer} />
+  
