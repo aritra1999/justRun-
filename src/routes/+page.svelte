4d2b4e1dee1a1eca6501ebae6f1307d5ba811445
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-02 11:39:38 +0100

    Format root page component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,23 @@
 <script lang="ts">
-	import Editor from "$lib/components/editor/editor.svelte";
-    export let data;
-    let content = data.content
+	import Editor from '$lib/components/editor/editor.svelte';
+	export let data;
+	let content = data.content;
 </script>
 
-
 <div class="h-screen w-screen flex">
-    <div class="h-screen w-1/2 p-3 pr-0">
-        {#if data.content}
-            <Editor bind:content />
-        {:else}
-            Loading
-        {/if}  
-    </div>
-    <div class="h-screen w-1/2 flex flex-col p-3">        
-        <div class=" h-1/2 pb-3">
-            <div class="bg-blue-100 w-full h-full rounded-lg">
-                Input
-            </div>
-        </div>
-        <div class="h-1/2">
-            <div class="bg-blue-100 w-full h-full rounded-lg">
-                Output
-            </div>
-        </div>
-    </div>
+	<div class="h-screen w-1/2 p-3 pr-0">
+		{#if data.content}
+			<Editor bind:content />
+		{:else}
+			Loading
+		{/if}
+	</div>
+	<div class="h-screen w-1/2 flex flex-col p-3">
+		<div class=" h-1/2 pb-3">
+			<div class="bg-blue-100 w-full h-full rounded-lg">Input</div>
+		</div>
+		<div class="h-1/2">
+			<div class="bg-blue-100 w-full h-full rounded-lg">Output</div>
+		</div>
+	</div>
 </div>
