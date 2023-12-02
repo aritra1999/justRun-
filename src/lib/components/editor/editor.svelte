e1f48d8383f6149643602a51f5e6366c4e6d7b4b
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-02 12:33:58 +0100

    Add JetBrains Mono and Space Grotesk

--- src/lib/components/editor/editor.svelte
+++ src/lib/components/editor/editor.svelte
@@ -1,36 +1,46 @@
-<script lang="ts">  
-  import loader from '@monaco-editor/loader';
-  import { onDestroy, onMount } from 'svelte';
-  import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
-  
-  export let content: string;
-  
-  let editor: Monaco.editor.IStandaloneCodeEditor;
-  let monaco: typeof Monaco;
-  let editorContainer: HTMLElement;
-  let options = {
-    theme: 'vs-light',
-    automaticLayout: true,
-    minimap: { enabled: false },
-  }
+<script lang="ts">
+	import loader from '@monaco-editor/loader';
+	import { onDestroy, onMount } from 'svelte';
+	import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
-  onMount(async () => {
-        const monacoEditor = await import('monaco-editor');
-        loader.config({ monaco: monacoEditor.default });
+	export let content: string;
 
-        monaco = await loader.init();
+	let editor: Monaco.editor.IStandaloneCodeEditor;
+	let monaco: typeof Monaco;
+	let editorContainer: HTMLElement;
+	let options = {
+		fontSize: 14,
+		fontFamily: 'JetBrains Mono',
+		fontLigatures: true,
+		roundedSelection: true,
+		scrollBeyondLastLine: false,
+		theme: 'vs-light',
+		automaticLayout: true,
+		minimap: { enabled: false }
+	};
 
-        const editor = monaco.editor.create(editorContainer, options);
-        const model = monaco.editor.createModel(content, 'cpp');
-        
-        editor.setModel(model);
-    });
+	onMount(async () => {
+		const monacoEditor = await import('monaco-editor');
+		loader.config({ monaco: monacoEditor.default });
 
-    onDestroy(() => {
-        monaco?.editor.getModels().forEach((model) => model.dispose());
-        editor?.dispose();
-    });
+		monaco = await loader.init();
+
+		const editor = monaco.editor.create(editorContainer, options);
+		const model = monaco.editor.createModel(content, 'cpp');
+
+		editor.setModel(model);
+	});
+
+	onDestroy(() => {
+		monaco?.editor.getModels().forEach((model) => model.dispose());
+		editor?.dispose();
+	});
 </script>
 
-<div class="h-full w-full rounded-lg" bind:this={editorContainer} />
-  
+<div class="h-full w-full rounded-lg editor" bind:this={editorContainer} />
+
+<style>
+	.editor {
+		font-family: 'JetBrains Mono', monospace !important;
+	}
+</style>
