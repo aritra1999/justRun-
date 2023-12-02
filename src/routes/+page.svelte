1709e9020c026b2c2cd3252599fba7d2b7fc6d39
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-02 11:27:44 +0100

    Use editor to render content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-
+	import Editor from "$lib/components/editor/editor.svelte";
+    export let data;
+    let content = data.content
 </script>
+
 
 <div class="h-screen w-screen flex">
     <div class="h-screen w-1/2 p-3 pr-0">
-        <div class="bg-blue-100 h-full w-full rounded-lg">
-
-        </div>
+        {#if data.content}
+            <Editor bind:content />
+        {:else}
+            Loading
+        {/if}  
     </div>
-    <div class="h-screen w-1/2 flex flex-col p-3">
-        <div class="bg-blue-100 h-20 rounded-lg mb-3">
-            Controls
-        </div>
-        <div class="h-full ">
-            <div class=" h-1/2 pb-3">
-                <div class="bg-blue-100 w-full h-full rounded-lg">
-                    Input
-                </div>
+    <div class="h-screen w-1/2 flex flex-col p-3">        
+        <div class=" h-1/2 pb-3">
+            <div class="bg-blue-100 w-full h-full rounded-lg">
+                Input
             </div>
-            <div class="h-1/2">
-                <div class="bg-blue-100 w-full h-full rounded-lg">
-                    Output
-                </div>
+        </div>
+        <div class="h-1/2">
+            <div class="bg-blue-100 w-full h-full rounded-lg">
+                Output
             </div>
         </div>
     </div>
