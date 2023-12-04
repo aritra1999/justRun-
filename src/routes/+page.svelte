c93ee1fad4effd50f20507ee18a9f968a52a7a85
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-04 08:47:41 +0100

    Use store for code, input and language support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,29 @@
 	import Output from '$lib/components/io/output.svelte';
 	import Input from '$lib/components/io/input.svelte';
 	import Menu from '$lib/components/menu/menu.svelte';
+	import { codeStore } from '$lib/store/store.js';
+
 	export let data;
-	let content = data.content;
+	let code = data.content;
 	let language = 'cpp';
+	let input = '';
+
+	$: (code, language, input),
+		codeStore.set({
+			code,
+			language,
+			input
+		});
 </script>
 
 <Menu />
 <div class="h-screen w-screen flex">
-	<div class="h-screen w-1/2 p-3 pr-0 bg-background">
-		<Editor bind:content bind:language editable={true} />
+	<div class="h-screen w-1/2 p-3 pr-0">
+		<Editor bind:content={code} bind:language editable={true} />
 	</div>
 	<div class="h-screen w-1/2 flex flex-col p-3">
 		<div class=" h-1/2 pb-3">
-			<Input />
+			<Input bind:input />
 		</div>
 		<div class="h-1/2">
 			<Output />
